9d2e25c276b9c58d9112b0d1be053b22b70053a2
Animesh Joshi <animeshjoshi0404@gmail.com>
2025-02-06 11:52:34 +0530

    fifth commit

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const person = require('./models/person');
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
-require('dotenv').config();
-//importing routers
+require("dotenv").config();
+
+
 const personRoutes = require('./routes/personRoutes');
 
 
-//using routes
 app.use('/person', personRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 
